346867a6479191bd7c28b1ab9d5ed22d6a4c1b44
kazufoot21 <kazuhiromaeda@maedatomohironoMacBook-Pro.local>
2021-12-10 06:17:13 +0900

    first setup express mongoose

--- app.js
+++ app.js
@@ -1,2 +1,28 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const path = require('path');
+
+mongoose
+  .connect('mongodb://localhost:27017/movies', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB connection ok.');
+  })
+  .catch((e) => {
+    console.log('MongoDB connection error');
+    console.log(e);
+  });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/dog', (req, res) => {
+  res.send('wanwanwan');
+});
+
+app.listen(3000, () => {
+  console.log('port3000');
+});
