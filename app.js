cb5d9fbdc9d1299a54d65b423fad083b17efcc9f
kazufoot21 <kazuhiromaeda@maedatomohironoMacBook-Pro.local>
2021-12-10 07:06:06 +0900

    make list index page

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ mongoose
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-  res.send('wanwanwan');
+app.get('/products', async (req, res) => {
+  const products = await Product.find({});
+  res.render('products/index', { products });
 });
 
 app.listen(3000, () => {
