288e8653dddc3ca1bd94833a828052f6c7ed4273
kazufoot21 <kazuhiromaeda@maedatomohironoMacBook-Pro.local>
2021-12-10 08:34:24 +0900

    make update product page

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+const { render } = require('ejs');
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const Product = require('./models/product');
+const methodOverride = require('method-override');
 
 mongoose
   .connect('mongodb://localhost:27017/farmStand', {
@@ -20,6 +22,7 @@ mongoose
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/products', async (req, res) => {
   const products = await Product.find({});
@@ -40,6 +43,21 @@ app.get('/products/:id', async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
   res.render('products/detail', { product });
+});
+
+app.get('/products/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render('products/edit', { product });
+});
+
+app.put('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
 });
 
 app.listen(3000, () => {
